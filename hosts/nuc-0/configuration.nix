ec2087215055933d45cdbfa32a822bf791cf17e2
David Millman <dave@cs.unc.edu>
2026-04-20 14:24:40 -0600

    feat: add more structure to the config

--- hosts/nuc-0/configuration.nix
+++ hosts/nuc-0/configuration.nix
@@ -7,5 +7,43 @@
     ./hardware-configuration.nix
   ];
 
-  networking.hostName = "nuc";
+  # Bootloader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.useOSProber = true;
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set the name of the host
+  networking.hostName = "nuc-0";
+
+  # Set your time zone.
+  time.timeZone = "America/Denver";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Install firefox.
+  programs.firefox.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # Warning!! Before changing this value read the documentation for this option
+  system.stateVersion = "25.11"; # Did you read the comment?
+
 }

2c363ef5c8132ccbdb07b352b53df61c7094093b
David Millman <dave@cs.unc.edu>
2026-04-21 12:25:57 -0600

    feat: add dev shell for working on project

--- flake.nix
+++ flake.nix
@@ -7,6 +7,10 @@
 
   outputs =
     { self, nixpkgs, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
     {
       nixosConfigurations.nuc-0 = nixpkgs.lib.nixosSystem {
         specialArgs = { inherit inputs; };
@@ -19,6 +23,13 @@
         specialArgs = { inherit inputs; };
         modules = [
           ./hosts/laptop-0/configuration.nix
+        ];
+      };
+
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          just
+          git
         ];
       };
     };

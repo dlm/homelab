222e55ee0179c3ed2f0eaf62de8ed618ad8d9669
David Millman <dave@cs.unc.edu>
2026-04-20 23:35:54 -0600

    feat: trim to be a server

--- modules/common.nix
+++ modules/common.nix
@@ -19,20 +19,6 @@
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  services.displayManager.gdm.enable = true;
-  services.desktopManager.gnome.enable = true;
-
-  # Configure keymap in X11
-  services.xserver.xkb.layout = "us";
-  services.xserver.xkb.variant = "";
-
-  # Install firefox.
-  programs.firefox.enable = true;
-
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 

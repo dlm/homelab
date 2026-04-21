cde7bfafea09e329b74acaa73238493c88ca5378
David Millman <dave@cs.unc.edu>
2026-04-20 23:05:51 -0600

    fix: rebase

--- hosts/nuc-0/configuration.nix
+++ hosts/nuc-0/configuration.nix
@@ -17,6 +17,19 @@
   # Set the name of the host
   networking.hostName = "nuc-0";
 
+  # Set the power settings
+  services.logind = {
+    lidSwitch = "ignore";
+    lidSwitchDocked = "ignore";
+    extraConfig = ''
+      HandleSuspendKey=ignore
+      HandleHibernateKey=ignore
+      HandleLidSwitch=ignore
+      HandleLidSwitchDocked=ignore
+      IdleAction=ignore
+    '';
+  };
+
   # Before changing this value read the documentation for this option
   system.stateVersion = "25.11"; # Did you read the comment?
 }

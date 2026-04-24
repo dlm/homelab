a032d986128b049324b80f7d1bd4fe80f66c5bc1
David Millman <dave@cs.unc.edu>
2026-04-24 11:18:21 -0600

    feat: setup remote signing

--- modules/common.nix
+++ modules/common.nix
@@ -18,6 +18,12 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
+  # allow signed packages from my workstations
+  nix.settings.trusted-public-keys = [
+    "petrillo-1:gMx5QyIPG0o2pLP+W1JTTe5w2UwDYHPmwQie28Yx+tg="
+    "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+  ];
+
   # Install system packages
   environment.systemPackages = with pkgs; [
     git
